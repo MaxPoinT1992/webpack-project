457e1b35cfd0ca88c4826ec1341fc9d3381aca2a
Victor Ciobanu <maxpointgames2@gmail.com>
2022-01-03 22:46:03 +0000

    Images

--- webpack.config.js
+++ webpack.config.js
@@ -12,11 +12,21 @@ if (process.env.NODE_ENV === "production"){
 module.exports = {
     mode: mode,
 target:target,
+ output:{
+     assetModuleFilename:"images/[hash][ext][query]"
+ },
     module:{
         rules:[
             {
+                test: /\.(png|jpg|jpeg|gif|svg)$/i,
+                type:"asset"
+            },
+            {
                 test:/\.s?css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+                use: [
+                    {loader:MiniCssExtractPlugin.loader,
+                    options:{publicPath:""}}
+                    , "css-loader", "sass-loader"],
             },
             {
                 test:/\.jsx?$/,
