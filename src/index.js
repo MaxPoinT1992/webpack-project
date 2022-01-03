57805b423561d8b412228f1e94ddd0ca4e2256b9
Victor Ciobanu <maxpointgames2@gmail.com>
2022-01-03 21:18:48 +0000

    ADD babel to project

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ const elvenShieldRecipe = {
     ironIgot:1,
     refinedMoonstone:4
 }
+
+const elvenGauntletsRecipe = {
+    ...elvenShieldRecipe,leather:1,
+    refinedMoonstone:4,
+}
+elvenGauntletsRecipe
+
 console.log(elvenShieldRecipe)
+console.log(elvenGauntletsRecipe)
